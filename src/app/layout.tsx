378d0eee760ddd827186ef9ae53063d02585d92d
Rohit Dasgupta <rdg922@gmail.com>
2024-04-22 16:54:23 -0400

    Remove extra files + add headers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,9 +21,6 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <style>
-        @import url(https://fonts.googleapis.com/css2?family=Jersey+10&family=Jersey+20&display=swap);
-      </style>
       </head>
       <body className={`font-sans ${inter.variable}`}>
       <div className="jersey-10-regular w-full flex justify-center">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,8 +9,14 @@ export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-row justify-center gap-12 px-4 py-16 ">
-        <CellularAutomata/>
-        <WaveFunctionCollapse/>
+        <div>
+          <p className="w-full text-3xl text-center p-2">Cellular Automata</p>
+          <CellularAutomata/>
+        </div>
+        <div>
+          <p className="w-full text-3xl text-center p-2">Wave Function Collapse</p>
+          <WaveFunctionCollapse/>
+        </div>
       </div>
     </main>
   );

a5cef3967f251fee5f01f6f1bf34de1ee98916c6
Rohit Dasgupta <rdg922@gmail.com>
2024-04-22 16:44:18 -0400

    Change X/Y size to Column/Row Count

--- src/components/CellularAutomata.tsx
+++ src/components/CellularAutomata.tsx
@@ -94,10 +94,10 @@ function CaveGeneratorCanvas({ interval = 300 }) {
         <div>
              <canvas ref={canvasRef} width={CANVAS_WIDTH} height={CANVAS_HEIGHT} ></canvas>
             <div className="bg-blue-500 text-white py-2 px-4 rounded">
-              X size:
+              Row Count:
               <input type= "number" style={{width: "80px"}} className="px-4 text-black bg-transparent" value={rows} onChange={(e) => setRows(Number(e.target.value))}/>
               <br></br>
-              Y size:
+              Column Count:
               <input type= "number" style={{width: "80px"}} className="px-4 text-black bg-transparent" value={cols} onChange={(e) => setCols(Number(e.target.value))}/>
               <br></br>
             Initial generation probability: 
